269d26d7678b9e1f55a09e8afef06aa4792fbf8a
Max Flores <max@maxflores.dev>
2022-07-11 06:51:21 -0600

    Deployment 2

--- components/GraphCMS/GraphCMS.tsx
+++ components/GraphCMS/GraphCMS.tsx
@@ -3,8 +3,4 @@ import { GraphQLClient } from "graphql-request";
 const url: any = process.env.CONTENT_API;
 const token: any = process.env.TOKEN_API;
 
-export const graphcms = new GraphQLClient(url, {
-    headers: {
-        Authorization: token,
-    },
-});
+export const graphcms = new GraphQLClient("https://api-us-west-2.graphcms.com/v2/cl56w9xj445hn01upeb63h72u/master");
